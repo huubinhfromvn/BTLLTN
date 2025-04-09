45377a3d8e14d36f0a75d8ad1684dac56975162e
huubinhfromvn <nghuubinh3005@gmail.com>
2025-04-09 17:55:34 +0700

    sua lai code

--- findroot.c
+++ findroot.c
@@ -10,8 +10,8 @@ float derivative(Token *postfix, float x) {
 }
 
 // Phương pháp Newton-Raphson
-float newtonRaphson(Token *postfix, float guess) {
-    float x = guess;
+float newtonRaphson(Token *postfix) {
+    float x = 1.0; // Giá trị khởi tạo
     int d=0;
     for (int i = 0; i < MAX_ITER; i++) {
         float fx = evaluatePostfix(postfix, x);
@@ -19,7 +19,7 @@ float newtonRaphson(Token *postfix, float guess) {
         if (fabs(dfx) < EPSILON) {
             printf("Dao ham gan bang 0, khong the tiep tuc\n");
             printf("Nghiem la: %f\n", x);
-            printf("Total iterations: %d\n", i+1);
+            printf("So lan lap: %d\n", i+1);
             return x;
         }
         float x_new = x - fx / dfx;
@@ -27,44 +27,50 @@ float newtonRaphson(Token *postfix, float guess) {
         if (fabs(x_new - x) < 1e-6){
             printf("fabs(x_new - x) la: %f\n", fabs(x_new - x));
             printf("Khoang cach giua x va xnew be hon 1e-6\n");
-            printf("Total iterations: %d\n", i+1);
+            printf("So lan lap: %d\n", i+1);
             return x_new;
         } 
         x = x_new;
         d++;
     }
-    printf("Total iterations: %d\n", d);
+    x=-1;
+    for (int i = 0; i < MAX_ITER; i++) {
+        float fx = evaluatePostfix(postfix, x);
+        float dfx = derivative(postfix, x);
+        if (fabs(dfx) < EPSILON) {
+            printf("Dao ham gan bang 0, khong the tiep tuc\n");
+            printf("Nghiem la: %f\n", x);
+            printf("So lan lap: %d\n", i+1);
+            return x;
+        }
+        float x_new = x - fx / dfx;
+        // printf("Iteration %d: x = %f, x_new = %f, fabs(x_new - x) = %e\n", i + 1, x, x_new, fabs(x_new - x));
+        if (fabs(x_new - x) < 1e-6){
+            printf("fabs(x_new - x) la: %f\n", fabs(x_new - x));
+            printf("Khoang cach giua x va xnew be hon 1e-6\n");
+            printf("So lan lap: %d\n", i+1);
+            return x_new;
+        } 
+        x = x_new;
+        d++;
+    }
+    printf("So lan lap: %d\n", d);
     return x;
 }
 
-// Phương pháp chia đôi (Bisection)
-// float bisectionMethod(Token *postfix) {
-//     float a = -2, b = 2; // Giả sử khoảng tìm nghiệm
-//     if (evaluatePostfix(postfix, a) * evaluatePostfix(postfix, b) >= 0) return NAN;
-    
-//     float c;
-//     for (int i = 0; i < MAX_ITER; i++) {
-//         c = (a + b) / 2.0;
-//         if (fabs(evaluatePostfix(postfix, c)) < EPSILON) return c;
-//         if (evaluatePostfix(postfix, a) * evaluatePostfix(postfix, c) < 0) b = c;
-//         else a = c;
-//     }
-//     return c;
-// }
-
 // Phương pháp Secant
-float secantMethod(Token *postfix) {
-    float x0 = -3, x1 = 3;
+float secantMethod(Token *postfix, float a, float b) {
+    float x0 = a, x1 = b;
     for (int i = 0; i < MAX_ITER; i++) {
         float f0 = evaluatePostfix(postfix, x0);
         float f1 = evaluatePostfix(postfix, x1);
         if (fabs(f1 - f0) < EPSILON){
             printf("Khoang cach giua f1 va f0 be hon 1e-4\n");
-            printf("Total iterations: %d\n", i+1);
+            printf("So lan lap: %d\n", i+1);
             return x1;
         }
         float x_new = x1 - f1 * (x1 - x0) / (f1 - f0);
-        printf("Iteration %d: x0 = %f, x1 = %f, x_new = %f\n", i + 1, x0, x1, x_new);
+        printf("Lan Lap %d: x0 = %f, x1 = %f, x_new = %f\n", i + 1, x0, x1, x_new);
         if (fabs(x_new - x1) < EPSILON){
             printf("Khoang cach giua x va xnew be hon 1e-4\n");
             return x_new;
--- findroot.h
+++ findroot.h
@@ -6,8 +6,8 @@
 #define MAX_ITER 1000    // Số lần lặp tối đa
 #define EPSILON 1e-4     // Ngưỡng sai số chấp nhận được
 
-float newtonRaphson(Token *postfix, float guess);
+float newtonRaphson(Token *postfix);
 float bisectionMethod(Token *postfix);
-float secantMethod(Token *postfix);
+float secantMethod(Token *postfix,float a, float b);
 
 #endif // FINDROOT_H
